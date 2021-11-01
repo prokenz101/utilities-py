46372364e54115c2b0f361137b13021cae94757d
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-11-01 07:34:56 +0400

    fixed grammar of remind()

--- functions.py
+++ functions.py
@@ -203,9 +203,9 @@ If you do not know how to use this command, try running 'help randnum'.""", 5])
 def reminder() -> None:
     esc()
     def remind_notif(message, singular):
-        if singular == True and message == None:
+        if singular and message == None:
             sentence = f"Hey! You set a reminder for {argv[2][:-1]} {time_options[i][1]} and its time!"
-        elif singular == False and message == None:
+        elif not singular and message == None:
             sentence = f"Hey! You set a reminder for {argv[2][:-1]} {time_options[i][1]}s and its time!"
         elif not message == None: sentence = f"Hey! Your reminder was: {message}"
         notification("Reminder!", sentence, 5)
@@ -216,10 +216,12 @@ If you don't know how to use the command, then run 'help remind'.""", 5])
     if message == "": message = None    
     time_options = {"s": (1, "second"), "m": (60, "minute"), "h": (3600, "hour")}
     try:
-        if float(argv[2][:-1]) == 1: one = True
+        if float(argv[2][:-1]) == 1.0:
+            one = True
+        else:
+            one = False
     except ValueError:
         notification("Huh.", "It seems that you have not inputted a number.", 5)
-    else: one = False
 
     for i in time_options:
         if argv[2].endswith(i):
