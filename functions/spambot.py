7e7f0d2cd81788c4026293a53d4ac40aaf3b8d6e
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2021-11-15 18:10:47 +0400

    added typing speed to spam

--- functions/spambot.py
+++ functions/spambot.py
@@ -1,4 +1,5 @@
 def spambot() -> None:
+    from pyautogui import FailSafeException
     from .basicfunctions import (
         notification,
         argv,
@@ -6,14 +7,16 @@ def spambot() -> None:
         hotkey,
         sleep,
     )
-    from pyautogui import FailSafeException
 
     number = argv[2]
+    typingspeed = argv[3]
     interval_list = argv[::-1]
-    word = argv[3:]
+    word = argv[4:]
 
+    if typingspeed == "fast":
+        typingspeed = 0
     if "--interval=" in " ".join(word[::-1]):
-        word = argv[3:-1]
+        word = argv[4:-1]
     if argv[2] == "infinite":
         number = 999999999999  # 999 billion is basically infinity like cmon no one is gonna get that far
     interval = 0
@@ -23,7 +26,7 @@ def spambot() -> None:
     try:
         notification("Spamming.", "Move mouse to corner of screen to stop.", 3)
         for i in range(int(number)):
-            typewrite(" ".join(word))
+            typewrite(" ".join(word), interval=typingspeed)
             hotkey("enter")
             sleep(interval)
     except FailSafeException:
